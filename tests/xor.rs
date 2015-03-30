50105edde71a984c4ea7398eb8c26b99826e4194
Jack Montgomery <jackm321@gmail.com>
2015-03-30 03:39:24 -0400

    added batch learning and json encoding to xor test

--- tests/xor.rs
+++ tests/xor.rs
@@ -2,7 +2,7 @@ extern crate nn;
 extern crate time;
 
 use std::num::Float;
-use nn::{NN, HaltCondition};
+use nn::{NN, HaltCondition, LearningMode};
 use time::Duration;
 
 #[test]
@@ -16,18 +16,23 @@ fn xor_timed() {
     ];
 
     // create a new neural network
-    let mut net = NN::new(&[2,3,1]);
+    let mut net1 = NN::new(&[2,3,1]);
 
     // train the network
-    net.train(&examples)
+    net1.train(&examples)
         .halt_condition( HaltCondition::Timer(Duration::seconds(10)) )
         .log_interval(None)
+        .learning_mode( LearningMode::Batch(2) )
         .momentum(0.1)
         .go();
 
+    // make sure json encoding/decoding works as expected
+    let json = net1.to_json();
+    let net2 = NN::from_json(&json);
+
     // test the trained network
     for &(ref inputs, ref outputs) in examples.iter() {
-        let results = net.run(inputs);
+        let results = net2.run(inputs);
         let (result, key) = (Float::round(results[0]), outputs[0]);
         assert!(result == key);
     }
