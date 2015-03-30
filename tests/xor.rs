be05a5469eb146ecfa882e87e962c0b1ba156f09
Jisoo Park <xxxyel@gmail.com>
2015-03-30 15:06:34 +0900

    Update to the latest rust nightly

--- tests/xor.rs
+++ tests/xor.rs
@@ -1,9 +1,9 @@
-#![feature(std_misc)]
-
 extern crate nn;
+extern crate time;
+
 use std::num::Float;
 use nn::{NN, HaltCondition};
-use std::time::Duration;
+use time::Duration;
 
 #[test]
 fn xor_timed() {
@@ -17,14 +17,14 @@ fn xor_timed() {
 
     // create a new neural network
     let mut net = NN::new(&[2,3,1]);
-    
+
     // train the network
     net.train(&examples)
         .halt_condition( HaltCondition::Timer(Duration::seconds(10)) )
         .log_interval(None)
         .momentum(0.1)
         .go();
-    
+
     // test the trained network
     for &(ref inputs, ref outputs) in examples.iter() {
         let results = net.run(inputs);
